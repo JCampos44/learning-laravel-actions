6878bc6584772a5a749f494d77b4ba22b161152f
JCampos44 <jecv.dev@gmail.com>
2026-04-17 14:07:47 -0400

    feat(todos): create UpdateTodoRequest for validating todo updates

---
+++ app/Http/Requests/V1/Todo/UpdateTodoRequest.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\V1\Todo;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateTodoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => ['sometimes', 'string', 'max:255'],
+            'description' => ['nullable', 'string'],
+            'status' => ['sometimes', 'in:pending,completed'],
+            'completed_at' => ['nullable', 'date'],
+        ];
+    }
+}
